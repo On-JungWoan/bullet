949dcf20ebf9d43b33e2602bd2e4c138cfa0a00c
jinwooseok <jinus7949@naver.com>
2023-07-15 18:50:10 +0900

    rename: 파일명 변경 delete: article폴더제거 feat: 테이블model추가

---
+++ backend/post/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from service.models import Site, Keyword
+# Create your models here.
+def Post(models):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    url = models.CharField(max_length=200)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    keyword = models.ForeignKey(Keyword, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- backend/service/models.py
+++ backend/service/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+def Subscribe(models):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    end_at = models.DateTimeField()
+
+def Category(model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+def Site(model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+def Keyword(model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
