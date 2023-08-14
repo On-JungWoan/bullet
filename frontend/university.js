83029a2dd22ff81f6f7e44bf5b66f5bab1fa1db1
jinwooseok <jinus7949@naver.com>
2023-08-14 17:54:34 +0900

    뉴스 로직 수정 및 프론트 연동

--- frontend/university.js
+++ frontend/university.js
@@ -4,21 +4,21 @@ export const universityData = [
         "src": require("./assets/university/강원대.png"),
         "site": "강원대"
     },
-    {
-        "id": "경북대",
-        "src": require("./assets/university/경북대.jpg"),
-        "site": "경북대"
-    },
+    // {
+    //     "id": "경북대",
+    //     "src": require("./assets/university/경북대.jpg"),
+    //     "site": "경북대"
+    // },
     {
         "id": "부산대",
         "src": require("./assets/university/부산대.png"),
         "site": "부산대"
     },
-    {
-        "id": "서울대",
-        "src": require("./assets/university/서울대.jpg"),
-        "site": "서울대"
-    },
+    // {
+    //     "id": "서울대",
+    //     "src": require("./assets/university/서울대.jpg"),
+    //     "site": "서울대"
+    // },
     {
         "id": "전남대",
         "src": require("./assets/university/전남대.png"),
@@ -29,16 +29,16 @@ export const universityData = [
         "src": require("./assets/university/전북대.png"),
         "site": "전북대"
     },
-    {
-        "id": "제주대",
-        "src": require("./assets/university/제주대.jpg"),
-        "site": "제주대"
-    },
-    {
-        "id": "충남대",
-        "src": require("./assets/university/충남대.jpg"),
-        "site": "충남대"
-    },
+    // {
+    //     "id": "제주대",
+    //     "src": require("./assets/university/제주대.jpg"),
+    //     "site": "제주대"
+    // },
+    // {
+    //     "id": "충남대",
+    //     "src": require("./assets/university/충남대.jpg"),
+    //     "site": "충남대"
+    // },
     {
         "id": "충북대",
         "src": require("./assets/university/충북대.png"),
