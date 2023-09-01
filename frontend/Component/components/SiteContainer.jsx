95c3a8864cab9efa908ba4c9867cfe9744bb1891
Seungmani <dkelcncjs@naver.com>
2023-09-01 09:51:46 +0900

    [refactor] 디자인 수정

--- frontend/Component/components/SiteContainer.jsx
+++ frontend/Component/components/SiteContainer.jsx
@@ -1,88 +1,127 @@
 // basic
 import React, { useEffect, useState, useContext, useCallback } from "react";
 import {
-    Text, View, StyleSheet, Image, ScrollView, Pressable, KeyboardAvoidingView
-} from 'react-native';
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  ScrollView,
+  Pressable,
+  KeyboardAvoidingView,
+} from "react-native";
 
 // install
 import { useNavigation } from "@react-navigation/native";
 
 // from App.js
 
-export default function SitesSelectPage({ transData,  transSite, setTransSite, postSite }) {
+export default function SitesSelectPage({
+  width,
+  transData,
+  transSite,
+  setTransSite,
+  postSite,
+}) {
+  const navigation = useNavigation();
+  console.log(width);
+  return (
+    <View>
+      <ScrollView
+        style={{ ...styles.sitesContainer }}
+        persistentScrollbar={true}
+      >
+        <KeyboardAvoidingView
+          behavior={Platform.select({ ios: "padding", android: undefined })}
+          style={{ ...styles.site }}
+        >
+          {transData.map((post, index) => {
+            return (
+              <Pressable
+                style={{ marginBottom: 5 }}
+                key={post.id}
+                onPress={() => {
+                  if (!transSite.includes(post.site)) {
+                    setTransSite([...transSite, post.site]);
+                  } else {
+                    let deleteSite = transSite;
+                    deleteSite.splice(deleteSite.indexOf(post.site), 1);
+                    setTransSite([...deleteSite]);
+                  }
+                }}
+              >
+                <Image
+                  style={{
+                    width: width,
+                    height: 80,
+                    resizeMode: "stretch",
+                    marginBottom: 3,
+                  }}
+                  source={post.src}
+                />
+                <Text style={{ fontSize: 15, textAlign: "center" }}>
+                  {post.site}
+                </Text>
+              </Pressable>
+            );
+          })}
+        </KeyboardAvoidingView>
+      </ScrollView>
 
-    const navigation = useNavigation();
+      <View style={{ ...styles.buttonContainer }}>
+        <Pressable
+          style={{ ...styles.button }}
+          onPress={() => {
+            navigation.pop();
+          }}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>이전 화면</Text>
+        </Pressable>
 
-    return (
-        <View>
-            <ScrollView style={{ ...styles.sitesContainer }} persistentScrollbar={true}>
-                <KeyboardAvoidingView behavior={Platform.select({ ios: 'padding', android: undefined })} style={{ ...styles.site }}>
-                    {transData.map((post, index) => {
-                        return (
-                            <Pressable style={{ marginBottom: 5 }} key={post.id} onPress={
-                                () => {
-                                    if (!transSite.includes(post.site)) {
-                                        setTransSite([...transSite, post.site]);
-                                    } else {
-                                        let deleteSite = transSite;
-                                        deleteSite.splice(deleteSite.indexOf(post.site), 1);
-                                        setTransSite([...deleteSite]);
-                                    }
-                                }
-                            }>
-                                <Image style={{ width: 100, height: 80, resizeMode: "stretch", marginBottom: 3 }} source={post.src} />
-                                <Text style={{ fontSize: 15, textAlign: 'center' }}>{post.site}</Text>
-                            </Pressable>
-                        );
-                    })}
-                </KeyboardAvoidingView>
-            </ScrollView>
-
-            <View style={{ ...styles.buttonContainer }}>
-                <Pressable style={{ ...styles.button }} onPress={() => { navigation.pop() }}>
-                    <Text style={{ color: "white", textAlign: 'center' }}>이전 화면</Text>
-                </Pressable>
-
-                <Pressable style={{ ...styles.button }} onPress={() => { postSite() }}>
-                    <Text style={{ color: "white", textAlign: 'center' }}>등록하기</Text>
-                </Pressable>
-            </View>
-            <View>
-                {transSite?.length ? <Text style={{ fontSize: 20 }}>{`선택 : ${transSite}`}</Text> : null}
-            </View>
-        </View>
-    )
-
+        <Pressable
+          style={{ ...styles.button }}
+          onPress={() => {
+            postSite();
+          }}
+        >
+          <Text style={{ color: "white", textAlign: "center" }}>등록하기</Text>
+        </Pressable>
+      </View>
+      <View>
+        {transSite?.length ? (
+          <Text style={{ fontSize: 20 }}>{`선택 : ${transSite}`}</Text>
+        ) : null}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    sitesContainer: {
-        borderWidth: 2,
-        marginTop: 10,
-    },
-    site: {
-        flexDirection: 'row',
-        flexWrap: "wrap",
-        paddingHorizontal: 16,
-        paddingVertical: 10,
-        justifyContent: "space-between",
-    },
-    buttonContainer: {
-        flex: 1,
-        flexDirection: "row",
-        backgroundColor: "Red",
-        flexWrap: "wrap",
-        marginTop: 10,
+  sitesContainer: {
+    borderWidth: 2,
+    marginTop: 10,
+  },
+  site: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    justifyContent: "space-between",
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "Red",
+    flexWrap: "wrap",
+    marginTop: 10,
+  },
+  button: {
+    flex: 1,
+    backgroundColor: "black",
+    borderWidth: 1,
+    borderRadius: 20,
 
-    },
-    button: {
-        flex: 1,
-        backgroundColor: "black",
-        borderWidth: 1,
-        borderRadius: 20,
-
-        paddingHorizontal: 15,
-        paddingVertical: 8,
-        marginHorizontal: 10,
-    }
-})
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    marginHorizontal: 10,
+  },
+});
--- frontend/Component/pages/UniSite.jsx
+++ frontend/Component/pages/UniSite.jsx
@@ -1,15 +1,13 @@
 // basic
 import React, { useEffect, useState, useContext, useCallback } from "react";
-import {
-    Text, View, StyleSheet, TextInput, KeyboardAvoidingView
-} from 'react-native';
+import { Text, View, StyleSheet, TextInput, Dimensions } from "react-native";
 
 // install
 import axios from "axios";
 import { useNavigation } from "@react-navigation/native";
 
 // from App.js
-import { dataContext } from '../../App';
+import { dataContext } from "../../App";
 import { AddSITE } from "../../App";
 import { BaseURL } from "../../App";
 import { TOKEN } from "./Main";
@@ -20,127 +18,142 @@ import { universityData } from "../../university";
 // component
 import SitesSelectPage from "../components/SiteContainer";
 
+const windowWidth = Dimensions.get("window").width;
+
 export default function UniSite() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const { user,dispatch } = useContext(dataContext);
+  const { user, dispatch } = useContext(dataContext);
 
-    const [searchValue, setSearchValue] = useState(''); // 검색 값
-    const [keywords, setKeywords] = useState(user.keywords?.length ? [...user.keywords] : []);
+  const [searchValue, setSearchValue] = useState(""); // 검색 값
+  const [keywords, setKeywords] = useState(
+    user.keywords?.length ? [...user.keywords] : []
+  );
 
-    const [transSite, setTransSite] = useState([...user.uniSites]) // 선택한 사이트
+  const [transSite, setTransSite] = useState([...user.uniSites]); // 선택한 사이트
 
-    const postSite = async () => {
+  const postSite = async () => {
+    if (transSite.length === 0) {
+      alert("선택한 사이트가 없습니다.");
+      return;
+    }
 
-        if (transSite.length === 0) {
-            alert('선택한 사이트가 없습니다.');
-            return;
-        }
+    const data = {
+      sites: [...user.newsSites, ...user.workSites, ...transSite],
+    };
 
-        const data = {
-            sites: [...user.newsSites,...user.workSites, ...transSite],
-        }
+    console.log(data);
+    console.log("transSite", user.newsSites);
+    console.log("user.uniSites", transSite);
+    console.log("user.workSites", user.workSites);
 
-        console.log(data)
-        console.log("transSite",user.newsSites)
-        console.log("user.uniSites",transSite)
-        console.log("user.workSites",user.workSites)
+    dispatch({
+      type: AddSITE,
+      newsSites: user.newsSites,
+      uniSites: transSite,
+      workSites: user.workSites,
+    });
 
-        dispatch({
-            type: AddSITE,
-            newsSites: user.newsSites,
-            uniSites: transSite,
-            workSites: user.workSites,
+    try {
+      await axios
+        .post(`${BaseURL}/user/site/create/`, data, {
+          headers: {
+            Authorization: TOKEN,
+          },
+        })
+        .then(function (response) {
+          // console.log("SitesSelectPage", response.data);
+          navigation.navigate("Register");
+        })
+        .catch(function (error) {
+          alert("에러발생");
+          console.log("error", error);
+          throw error;
         });
+    } catch (error) {
+      console.log("error", error);
+      throw error;
+    }
+  };
 
-        try {
-            await axios
-                .post(`${BaseURL}/user/site/create/`, data, {
-                    headers: {
-                        Authorization: TOKEN,
-                    },
-                }
-                )
-                .then(function (response) {
-                    // console.log("SitesSelectPage", response.data);
-                    navigation.navigate('Register');
-                })
-                .catch(function (error) {
-                    alert("에러발생")
-                    console.log("error", error);
-                    throw error;
-                });
-        } catch (error) {
-            console.log("error", error);
-            throw error;
-        }
+  // 검색 기능
+  onChangeSearch = (e) => {
+    setSearchValue(e);
+  };
+
+  // enter 이벤트 미완성, 검색 값을 포함하는 결과를 보여줌
+  onSubmitText = () => {
+    if (searchValue === "") {
+      return;
     }
 
-    // 검색 기능
-    onChangeSearch = (e) => {
-        setSearchValue(e);
-    }
+    setKeywords([...keywords, searchValue]);
+    setSearchValue("");
+  };
 
-    // enter 이벤트 미완성, 검색 값을 포함하는 결과를 보여줌
-    onSubmitText = () => {
-        if (searchValue === '') {
-            return;
-        }
+  return (
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View style={{ flex: 1, width: "90%", marginLeft: "5%" }}>
+        <View style={{ ...styles.headContainer }}>
+          <Text style={{ ...styles.searchText }}>
+            원하는 사이트를 선택하세요
+          </Text>
 
-        setKeywords([...keywords, searchValue]);
-        setSearchValue('')
-    }
-
-    return (
-
-        <View style={{ flex: 1, width:'90%', marginLeft : '5%' }}>
-            <View style={{ ...styles.headContainer }}>
-                <Text style={{ ...styles.searchText }}>원하는 사이트를 선택하세요</Text>
-
-                <TextInput placeholder="사이트를 검색하세요" autoCapitalize="none" autoCorrect={false}
-                    style={{ ...styles.searchInput }} value={searchValue} onChangeText={onChangeSearch}
-                    onSubmitEditing={onSubmitText} />
-            </View>
-            <View style={{ ...styles.showSite }}>
-                <SitesSelectPage transData={universityData} transSite={transSite} setTransSite={setTransSite} postSite={postSite}/>
-            </View>
-            <View style={{ flex: 1 }}>
-
-            </View>
+          <TextInput
+            placeholder="사이트를 검색하세요"
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={{ ...styles.searchInput }}
+            value={searchValue}
+            onChangeText={onChangeSearch}
+            onSubmitEditing={onSubmitText}
+          />
         </View>
-    )
+        <View style={{ ...styles.showSite }}>
+          <SitesSelectPage
+            width={windowWidth * 0.3}
+            transData={universityData}
+            transSite={transSite}
+            setTransSite={setTransSite}
+            postSite={postSite}
+          />
+        </View>
+        <View style={{ flex: 1 }}></View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    headContainer: {
-        flex: 1.5,
-        justifyContent: 'center',
-        alignItems: 'center',
+  headContainer: {
+    flex: 1.5,
+    justifyContent: "center",
+    alignItems: "center",
 
-        marginTop: "15%",
-    },
-    showSite: {
-        flex: 7,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    arrow: {
-        position: 'absolute',
-        top: 25,
-        left: 25,
-    },
-    searchText: {
-        color: 'black',
-        fontSize: 20,
-        fontWeight: 700,
-        textAlign: 'center',
-    },
-    searchInput: {
-        textAlign: 'center',
-        fontSize: 20,
-        borderRadius: 10,
-        borderWidth: 1,
-        width: '80%',
-        marginTop: 10
-    }
-})
+    marginTop: "15%",
+  },
+  showSite: {
+    flex: 7,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  arrow: {
+    position: "absolute",
+    top: 25,
+    left: 25,
+  },
+  searchText: {
+    color: "black",
+    fontSize: 20,
+    fontWeight: 700,
+    textAlign: "center",
+  },
+  searchInput: {
+    textAlign: "center",
+    fontSize: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    width: "80%",
+    marginTop: 10,
+  },
+});
